ddb5a01d02f270fc4ca34ac472b35390621e2014
Rodrigo Oliveira <rodrigooliveira@geodados.com.br>
2022-02-01 09:33:48 -0300

    linhas adicionadas

--- teste_Js.js
+++ teste_Js.js
@@ -6,11 +6,11 @@
 
 //const fetch = require('node-fetch');
 
-// fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
-//   .then(function(response) {
-//     return response.json(); // converts response to json
-//   })
-//   .then(function(data) {
-//     // do something with data here
-//      console.log(data)
-//   });
+fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
+  .then(function(response) {
+    return response.json(); // converts response to json
+  })
+  .then(function(data) {
+    // do something with data here
+     console.log(data)
+  });
