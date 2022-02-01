103bee5d35933ab729d7c4edbc05502dcd910163
Rodrigo <rco.automacao@gmail.com>
2022-02-01 13:02:13 -0300

    inserção div 3

--- teste_Js.js
+++ teste_Js.js
@@ -6,13 +6,28 @@
 
 //const fetch = require('node-fetch');
 
-fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
-  .then(function(response) {
-    return response.json(); // converts response to json
-  })
-  .then(function(data) {
-    // do something with data here
-     console.log(data)
-     console.log(dados);
-     console.log(dados);
-  });
+// fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
+//   .then(function(response) {
+//     return response.json(); // converts response to json
+//   })
+//   .then(function(data) {
+//     // do something with data here
+//      console.log(data)
+//      console.log(dados);
+//      console.log(dados);
+//   });
+
+let getUsers = async () => {
+   
+  let response = await fetch("https://jsonplaceholder.typicode.com/users");   
+  let users = await response.json();
+  if(response.status == 200)
+  {
+      console.log(users);
+  }
+  else  if(response.status == 404)
+  {
+      console.log("erro !");
+  }   
+}
+getUsers();
