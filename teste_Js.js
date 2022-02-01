2b1965065bff607293b463adc4b117ecc17880b9
Rodrigo Oliveira <rodrigooliveira@geodados.com.br>
2022-02-01 09:28:46 -0300

    adição dos arquivos ao git

---
+++ teste_Js.js
@@ -0,0 +1,16 @@
+// $("#botao").click(()=>{
+   
+//   $("#botao").text("clicado");
+// })
+
+
+//const fetch = require('node-fetch');
+
+// fetch('http://worldtimeapi.org/api/timezone/Europe/Amsterdam')
+//   .then(function(response) {
+//     return response.json(); // converts response to json
+//   })
+//   .then(function(data) {
+//     // do something with data here
+//      console.log(data)
+//   });
